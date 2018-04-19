1e9d56010ea65278e3b0134362eff270b48f29f9
Sarkis M <melkonsa@gmail.com>
2018-04-18 21:34:38 -0700

    set up testing and added the beforeEach hook and one test...

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -8,7 +8,7 @@ contract CampaignFactory {
        deployedCampaigns.push(newCampaign);
     }
     
-    function getDeployedCampaings() public view returns(address[]) {
+    function getDeployedCampaigns() public view returns(address[]) {
         return deployedCampaigns;
     }
 }
